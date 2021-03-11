e94c6f32e7d71f2b153ec1cdf05bb75448cb8ea4
notGonim <mhmodgonim96@gmail.com>
2021-03-11 15:16:30 +0200

    ADDED the product model

---
+++ backend/models/product-model.js
@@ -0,0 +1,97 @@
+import mongoose from 'mongoose'
+
+
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please Enter Product Name '],
+            trim: true,
+            maxLength: [100, 'Product Name Must not Exceed 100 characters']
+        },
+        price: {
+            type: Number,
+            required: [true, 'Please Enter Product Price'],
+            maxLength: [100, 'Product Name Must not Exceed 100 characters'],
+            default: 0.0,
+        },
+        description: {
+            type: String,
+            required: [true, 'Please Enter Description'],
+        },
+        rating: {
+            type: Number,
+            default: 0.0
+        },
+        images: [
+            {
+                public_id: {
+                    type: String,
+                    required: true
+                },
+                url: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+        category: {
+            type: Number,
+            required: [true, 'Please Select Category for the product'],
+            enum: {
+                values: [
+                    'Electronics',
+                    'Cameras',
+                    'Phones',
+                    'Laptop',
+                    'Accessories',
+                    'Headphones',
+                    'Food',
+                    'Books',
+                    'Clothes/shoes',
+                    'Sports',
+                    'Home'
+                ],
+                message: 'Please Select Category for the Product'
+            }
+        },
+        seller: {
+            type: Number,
+            required: [true, 'Please the product Seller'],
+        },
+        stock: {
+            type: Number,
+            required: [true, 'Please Enter Product Stock'],
+            maxLength: [5, 'Product Stock  Must not Exceed 5 characters'],
+            default: 0
+        },
+        numberOfReviews: {
+            type: Number,
+            default: 0
+        },
+        reviews: [
+            {
+                name: {
+                    type: String,
+                    required: true
+                },
+                ratings: {
+                    type: Number,
+                    required: true
+                },
+                comment: {
+                    typeLString,
+                    required: true
+                }
+            }
+        ],
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+)
+
+const Product = mongoose.model('Products', productSchema)
+export default Product
