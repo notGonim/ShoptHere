cf6e747b9c6dd2dc112fa386be798ef01cae8119
notGonim <mhmodgonim96@gmail.com>
2021-03-12 14:55:40 +0200

    Added the condition to run different error wether its in prod or dev

--- backend/middleware/errors.js
+++ backend/middleware/errors.js
@@ -3,7 +3,31 @@ import { ErrorHandler } from "../utils/errorHandler.js"
 
 export const errors = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
-    err.message = err.message || 'Internal Server Error'
+
+    if (process.env.NODE_ENV === "DEVELOPMENT") {
+        res.status(err.statusCode).json(
+            {
+                success: false,
+                error: err,
+                errMessage: err.message,
+                stack: err.stack
+            }
+        )
+    }
+    if (process.env.NODE_ENV === "PRODUCTION") {
+        const  error = { ...err }
+        err.message = err.message
+
+        res.status(error.statusCode).json(
+            {
+                success: false,
+                message: error.message || 'Internal Server Error',
+ 
+            }
+        )
+    }
+
+
     res.status(err.statusCode).json(
         {
             success: false,
