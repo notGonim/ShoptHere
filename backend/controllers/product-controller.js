b987e92c6962d6dc765c6be20abe5609497891c4
notGonim <mhmodgonim96@gmail.com>
2021-03-12 13:33:44 +0200

    Added the functonality for UpdateProduct where you can update product by its id with new value

--- backend/controllers/product-controller.js
+++ backend/controllers/product-controller.js
@@ -1,33 +1,22 @@
-
-
 //this file is to handel all the login for the product router
-
 import Product from "../models/product-model.js"
 
-
-
-
-
-//to create a new product  -> /api/v1/product/new  
+//to create a new product  -> /api/v1/admin/product/new  
 export const newProduct = async (req, res, next) => {
 
     const product = await Product.create(req.body)
-    res.status(200).json(
+    res.status(201).json(
         {
             success: true,
             product
         }
     )
-
-
 }
-
 
 //to get all the products from -> /api/v1/product 
 export const getProducts = async (req, res, next) => {
 
     const products = await Product.find()
-
     res.status(200).json(
         {
             success: true,
@@ -37,16 +26,10 @@ export const getProducts = async (req, res, next) => {
     )
 }
 
-
-
-
-
 //to get a single product -> /api/v1/product/:id
-
 export const getProductById = async (req, res, next) => {
 
     const product = await Product.findById(req.params.id)
-
     if (!product) {
         return res.status(404).json(
             {
@@ -55,7 +38,6 @@ export const getProductById = async (req, res, next) => {
             }
         )
     }
-
     res.status(200).json(
         {
             success: true,
@@ -64,6 +46,29 @@ export const getProductById = async (req, res, next) => {
     )
 }
 
+//to update product  ->/api/v1/:id
+export const updateProductById = async (req, res, next) => {
 
+    let product = await Product.findById(req.params.id)
+    if (!product) {
+        return res.status(404).json(
+            {
+                success: false,
+                message: 'Product not Found'
+            }
+        )
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+    res.status(200).json(
+        {
+            success: true,
+            product
+        }
+    )
+}
 
 
--- backend/routes/product-route.js
+++ backend/routes/product-route.js
@@ -1,7 +1,7 @@
 //this file will handle all the routes for the product 
 
 import express from 'express'
-import { newProduct, getProducts, getProductById } from '../controllers/product-controller.js'
+import { newProduct, getProducts, getProductById, updateProductById } from '../controllers/product-controller.js'
 
 export const ProductRouter = express.Router()
 
@@ -9,6 +9,7 @@ export const ProductRouter = express.Router()
 
 
 ProductRouter.get('/products', getProducts)
+ProductRouter.put('/admin/product/:id', updateProductById)
 ProductRouter.get('/product/:id', getProductById)
-ProductRouter.post('/product/new', newProduct)
+ProductRouter.post('/admin/product/new', newProduct)
 
