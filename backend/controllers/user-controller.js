5f2e14a09b8b9ebb145128c54700aef12cab5580
notGonim <mhmodgonim96@gmail.com>
2021-03-14 20:47:15 +0200

    Added user control file

---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,18 @@
+import { asyncError } from "../middleware/catchAsyncErrors.js"
+import User from "../models/user-modek.js"
+import { ErrorHandler } from "../utils/errorHandler.js"
+
+
+//register a user => api/v1/register
+export const registerUser = asyncError(async (req, res, next) => {
+
+    const { name, email, password } = req.body
+    const user=await User.create(
+        {
+            name,
+            email,
+            password,
+            
+        }
+    )
+})
--- backend/models/user-modek.js
+++ backend/models/user-modek.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import validator from 'validator'
+
 
 const userSchema = new mongoose.Schema({
     name: {
