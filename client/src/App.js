f606cf04b68193dc3173267efad6b5496746f6df
notGonim <mhmodgonim96@gmail.com>
2021-03-15 04:24:00 +0200

    Added the main router for home

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import { Home } from "./components/Home";
 import { Footer } from "./components/layouts/Footer";
 import { Header } from "./components/layouts/Header";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <Router >
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Route path="/" exact component={Home} />
+        </div>
+        <Footer />
+      </div>
+    </Router >
   );
 }
 
