211c7f148c8e0e5c41414ae3a29d6c53b830e33f
notGonim <mhmodgonim96@gmail.com>
2021-03-15 04:33:50 +0200

    Added the Helmet for the browser

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,35 +1,37 @@
 import React from 'react'
+import { MetaData } from './layouts/MetaData'
 
 export const Home = () => {
-    return (
-        <div classNameName="container container-fluid">
-                <h1 id="products_heading">Latest Products</h1>
+  return (
+    <div classNameName="container container-fluid">
+      <MetaData title={"Buy Best Products Online"} />
+      <h1 id="products_heading">Latest Products</h1>
 
-                <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-          <div className="card p-3 rounded">
-            <img
-              className="card-img-top mx-auto"
-              src="https://m.media-amazon.com/images/I/61B04f0ALWL._AC_UY218_.jpg"
-            />
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">
-                <a href=""
-                  >Wyze Cam 1080p HD Indoor Wireless Smart Home Camera Wyze Cam 1080p HD Indoor Wireless Smart Home Camera</a
-                >
-              </h5>
-              <div className="ratings mt-auto">
-               <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star-half-o"></i>
-                <i className="fa fa-star-o"></i>
-                <span id="no_of_reviews">(5 Reviews)</span>
-              </div>
-              <p className="card-text">$965.67</p>
-              <a href="#" id="view_btn" className="btn btn-block">View Details</a>
+      <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+        <div className="card p-3 rounded">
+          <img
+            className="card-img-top mx-auto"
+            src="https://m.media-amazon.com/images/I/61B04f0ALWL._AC_UY218_.jpg"
+          />
+          <div className="card-body d-flex flex-column">
+            <h5 className="card-title">
+              <a href=""
+              >Wyze Cam 1080p HD Indoor Wireless Smart Home Camera Wyze Cam 1080p HD Indoor Wireless Smart Home Camera</a
+              >
+            </h5>
+            <div className="ratings mt-auto">
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star-half-o"></i>
+              <i className="fa fa-star-o"></i>
+              <span id="no_of_reviews">(5 Reviews)</span>
             </div>
+            <p className="card-text">$965.67</p>
+            <a href="#" id="view_btn" className="btn btn-block">View Details</a>
           </div>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
---
+++ client/src/components/layouts/MetaData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+
+export const MetaData = ({ title }) => {
+    return (
+        <Helmet>
+            <title>{title}-ShopHere</title>
+        </Helmet>
+    )
+}
