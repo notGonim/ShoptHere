8775a60aced91d424366597be68be6353dcc30d5
notGonim <mhmodgonim96@gmail.com>
2021-03-15 21:58:06 +0200

    Handle errors with duplicated mongoose

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -49,3 +49,46 @@ export const userLogout = asyncError(async (req, res, next) => {
         message: 'Logged Out'
     })
 })
+/*
+
+//forget password
+export const forgetPassword = asyncError(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next(new ErrorHandler('User not found with this email', 404));
+    }
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    // Create reset password url
+    const resetUrl = `${req.protocol}://${req.get('host')}/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`
+
+    try {
+
+        await sendEmail({
+            email: user.email,
+            subject: 'ShopHere Password Recovery',
+            message
+        })
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to: ${user.email}`
+        })
+
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(error.message, 500))
+    }
+})
+*/
--- backend/middleware/errors.js
+++ backend/middleware/errors.js
@@ -30,6 +30,25 @@ export const errors = (err, req, res, next) => {
             error = new ErrorHandler(message, 400)
         }
 
+        //handle mongoose duplicate error 
+        if (err.code === 11000) {
+            const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+            error = new ErrorHandler(message, 400)
+        }
+
+
+        // Handling wrong JWT error
+        if (err.name === 'JsonWebTokenError') {
+            const message = 'JSON Web Token is invalid. Try Again!!!'
+            error = new ErrorHandler(message, 400)
+        }
+
+        // Handling Expired JWT error
+        if (err.name === 'TokenExpiredError') {
+            const message = 'JSON Web Token is expired. Try Again!!!'
+            error = new ErrorHandler(message, 400)
+        }
+
         res.status(error.statusCode).json(
             {
                 success: false,
