52fccedd9cdcd31cb6a728f2bf580dce0b1b4eff
notGonim <mhmodgonim96@gmail.com>
2021-03-15 21:16:21 +0200

    Added the AuthorizesRoles function

--- backend/middleware/auth-routes.js
+++ backend/middleware/auth-routes.js
@@ -15,3 +15,14 @@ export const isUserAuthenticated = asyncError(async (req, res, next) => {
     req.user = await User.findById(decoded.id)
     next()
 })
+
+//Handle user roles 
+export const authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorHandler(`Role (${req.user.role}) is not allowed to access these resources `, 403)
+            )
+        }
+    }
+    next()
+}
