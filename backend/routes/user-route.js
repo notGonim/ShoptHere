99bb7f7a41bc78dec8a1fa9c0a988bb86e20e697
notGonim <mhmodgonim96@gmail.com>
2021-03-16 20:12:10 +0200

    Done with users routes

--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser, userLogin, getUserDetails, getAllUsers, updateProfile, updatePassword, getUserProfile, userLogout } from '../controllers/user-controller.js'
+import { registerUser, userLogin, getUserDetails, deleteUser, updateUser, getAllUsers, updateProfile, updatePassword, getUserProfile, userLogout } from '../controllers/user-controller.js'
 import { isUserAuthenticated, authorizeRoles } from '../middleware/auth-routes.js'
 
 export const UserRouter = express.Router()
@@ -12,7 +12,9 @@ UserRouter.get('/me', isUserAuthenticated, getUserProfile)
 UserRouter.put('/me/update', isUserAuthenticated, updateProfile)
 UserRouter.get('/password/update', isUserAuthenticated, updatePassword)
 UserRouter.get('/admin/users', isUserAuthenticated, authorizeRoles('admin'), getAllUsers)
-UserRouter.get('/admin/users', isUserAuthenticated, authorizeRoles('admin'), getUserDetails)
+UserRouter.get('/admin/user/:id', isUserAuthenticated, authorizeRoles('admin'), getUserDetails)
+UserRouter.delete('/admin/user/:id', isUserAuthenticated, authorizeRoles('admin'), deleteUser)
+UserRouter.put('/admin/user/:id', isUserAuthenticated, authorizeRoles('admin'), updateUser)
 
 
 
