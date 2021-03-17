a4994d3da09b12dd324d2ab90168d0cbcbd5d049
notGonim <mhmodgonim96@gmail.com>
2021-03-17 08:53:24 +0200

    Displaying products data in home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../redux/products/product-actions'
 import { MetaData } from './layouts/MetaData'
 
@@ -8,6 +8,8 @@ import { MetaData } from './layouts/MetaData'
 export const Home = () => {
 
   const dispatch = useDispatch()
+  const { loading, products, errors, productsCount } = useSelector(state => state.products)
+
 
   useEffect(() => {
     dispatch(getProducts())
@@ -20,31 +22,36 @@ export const Home = () => {
       <MetaData title={"Buy Best Products Online"} />
       <h1 id="products_heading">Latest Products</h1>
 
-      <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-        <div className="card p-3 rounded">
-          <img
-            className="card-img-top mx-auto"
-            src="https://m.media-amazon.com/images/I/61B04f0ALWL._AC_UY218_.jpg"
-          />
-          <div className="card-body d-flex flex-column">
-            <h5 className="card-title">
-              <a href=""
-              >Wyze Cam 1080p HD Indoor Wireless Smart Home Camera Wyze Cam 1080p HD Indoor Wireless Smart Home Camera</a
-              >
-            </h5>
-            <div className="ratings mt-auto">
-              <i className="fa fa-star"></i>
-              <i className="fa fa-star"></i>
-              <i className="fa fa-star"></i>
-              <i className="fa fa-star-half-o"></i>
-              <i className="fa fa-star-o"></i>
-              <span id="no_of_reviews">(5 Reviews)</span>
+      <section id="products" className="container mt-5">
+        <div className="row">
+          {products && products.map(product => (
+            <div key={product._id} className="col-sm-12 col-md-6 col-lg-3 my-3">
+              <div className="card p-3 rounded">
+                <img
+                  className="card-img-top mx-auto"
+                  src={product.images.[0].url}
+                />
+                <div className="card-body d-flex flex-column">
+                  <h5 className="card-title">
+                    <a href="#">{product.name}</a>
+                  </h5>
+                  <div className="ratings mt-auto">
+                    <div className="rating-outer">
+                      <div className="rating-inner" style={{
+                        width: `${(product.rating / 5) * 100}%`
+                      }}></div>
+                    </div>
+                    <span id="no_of_reviews">({product.numberOfReviews} Reviews)</span>
+                  </div>
+                  <p className="card-text">${product.price}</p>
+                  <a href="#" id="view_btn" className="btn btn-block">View Details</a>
+                </div>
+              </div>
             </div>
-            <p className="card-text">$965.67</p>
-            <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-          </div>
+          ))}
+
         </div>
-      </div>
+      </section>
     </div>
   )
 }
