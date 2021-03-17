0d52fe90a1634f8e1ed318767770a547d79f5ce6
notGonim <mhmodgonim96@gmail.com>
2021-03-17 08:58:23 +0200

    Splitting code so product now has its own components

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../redux/products/product-actions'
 import { MetaData } from './layouts/MetaData'
+import { Product } from './product/Product'
 
 
 
@@ -25,29 +26,7 @@ export const Home = () => {
       <section id="products" className="container mt-5">
         <div className="row">
           {products && products.map(product => (
-            <div key={product._id} className="col-sm-12 col-md-6 col-lg-3 my-3">
-              <div className="card p-3 rounded">
-                <img
-                  className="card-img-top mx-auto"
-                  src={product.images.[0].url}
-                />
-                <div className="card-body d-flex flex-column">
-                  <h5 className="card-title">
-                    <a href="#">{product.name}</a>
-                  </h5>
-                  <div className="ratings mt-auto">
-                    <div className="rating-outer">
-                      <div className="rating-inner" style={{
-                        width: `${(product.rating / 5) * 100}%`
-                      }}></div>
-                    </div>
-                    <span id="no_of_reviews">({product.numberOfReviews} Reviews)</span>
-                  </div>
-                  <p className="card-text">${product.price}</p>
-                  <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-                </div>
-              </div>
-            </div>
+            <Product key={product._id} product={product} />
           ))}
 
         </div>
