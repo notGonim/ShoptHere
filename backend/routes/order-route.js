502c40e8e9d0e5cc3592fe9eb472814f537c37a0
notGonim <mhmodgonim96@gmail.com>
2021-03-18 12:55:57 +0200

    ADDED the route for user to delete a certain order

--- backend/routes/order-route.js
+++ backend/routes/order-route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { newOrder, getSingleOrder, myOrder, allOrder ,updateOrder } from '../controllers/order-controler.js'
+import { newOrder, getSingleOrder, myOrder, allOrder, updateOrder deleteOrder } from '../controllers/order-controler.js'
 import { isUserAuthenticated, authorizeRoles } from '../middleware/auth-routes.js'
 
 
@@ -11,4 +11,5 @@ OrderRouter.get('/order/:id', isUserAuthenticated, getSingleOrder)
 OrderRouter.get('/orders/me', isUserAuthenticated, myOrder)
 OrderRouter.get('/admin/orders', isUserAuthenticated, authorizeRoles('admin'), allOrder)
 OrderRouter.put('/admin/order/:id', isUserAuthenticated, authorizeRoles('admin'), updateOrder)
+OrderRouter.delete('/admin/order/:id', isUserAuthenticated, authorizeRoles('admin'), deleteOrder)
 
