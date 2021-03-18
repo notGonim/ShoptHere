6c60e8a0bb9b45be5ce0b541f203f90dece49190
notGonim <mhmodgonim96@gmail.com>
2021-03-18 12:12:20 +0200

    Create the order router file which handle all order routes

---
+++ backend/routes/order-route.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { newOrder } from '../controllers/order-controler'
+import { isUserAuthenticated, authorizeRoles } from '../middleware/auth-routes.js'
+
+
+export const OrderRouter = express.Router()
+
+
+OrderRouter.post('/order/new',newOrder)
