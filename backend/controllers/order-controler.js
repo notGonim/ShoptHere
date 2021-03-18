494b9112e6bf50befdc0e2e2ba409c747dc7d5fb
notGonim <mhmodgonim96@gmail.com>
2021-03-18 12:10:19 +0200

    ADDED newOrder function which enables user to create new order

--- backend/controllers/order-controler.js
+++ backend/controllers/order-controler.js
@@ -16,10 +16,23 @@ export const newOrder = asyncError(async (req, res, next) => {
         shippingPrice,
         totalPrice,
         paymentInfo } = req.body
-        
 
+    const order = await Order.create({
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo,
+        paidAt: Date.now(),
+        user: req.user._id
+    })
+    res.status(200).json({
+        success: true,
+        order
+    })
 
-        
 })
 
 
