a21236a13d58c3df0faff9e121f05b55d1276750
notGonim <mhmodgonim96@gmail.com>
2021-03-20 09:00:47 +0200

    ADDED the LOGIN func

--- client/src/redux/user/user-actions.js
+++ client/src/redux/user/user-actions.js
@@ -1,1 +1,28 @@
 
+import axios from 'axios'
+
+
+// login func 
+export const login = (email, password) => async (dispatch) => {
+    try {
+
+        dispatch({ type: LOGIN_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const { data } = await axios.post('/api/login', { email, password }, config)
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: data.user
+        })
+
+    } catch (err) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: err.response.data.message
+        })
+    }
+}
