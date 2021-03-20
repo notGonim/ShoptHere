802cbde3fe40d9eff2fbd035ac3168aa4ef7d4c0
notGonim <mhmodgonim96@gmail.com>
2021-03-20 09:43:20 +0200

    ADDED the design for the register component

--- client/src/components/user/Register.js
+++ client/src/components/user/Register.js
@@ -46,8 +46,88 @@ export const Register = ({ history }) => {
     }
 
     return (
-        <div>
+        <>
+            <div className="row wrapper">
+                <div className="col-10 col-lg-5">
+                    <form className="shadow-lg" onSubmit={submitHandler} encType='multipart/form-data'>
+                        <h1 className="mb-3">Register</h1>
 
-        </div>
+                        <div className="form-group">
+                            <label htmlFor="email_field">Name</label>
+                            <input
+                                type="name"
+                                id="name_field"
+                                className="form-control"
+                                name='name'
+                                value={name}
+                                onChange={onChange}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="email_field">Email</label>
+                            <input
+                                type="email"
+                                id="email_field"
+                                className="form-control"
+                                name='email'
+                                value={email}
+                                onChange={onChange}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password_field">Password</label>
+                            <input
+                                type="password"
+                                id="password_field"
+                                className="form-control"
+                                name='password'
+                                value={password}
+                                onChange={onChange}
+                            />
+                        </div>
+
+                        <div className='form-group'>
+                            <label htmlFor='avatar_upload'>Avatar</label>
+                            <div className='d-flex align-items-center'>
+                                <div>
+                                    <figure className='avatar mr-3 item-rtl'>
+                                        <img
+                                            src={avatarPreview}
+                                            className='rounded-circle'
+                                            alt='Avatar Preview'
+                                        />
+                                    </figure>
+                                </div>
+                                <div className='custom-file'>
+                                    <input
+                                        type='file'
+                                        name='avatar'
+                                        className='custom-file-input'
+                                        id='customFile'
+                                        accept="iamges/*"
+                                        onChange={onChange}
+                                    />
+                                    <label className='custom-file-label' htmlFor='customFile'>
+                                        Choose Avatar
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <button
+                            id="register_button"
+                            type="submit"
+                            className="btn btn-block py-3"
+                            disabled={loading ? true : false}
+                        >
+                            REGISTER
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+        </>
     )
 }
