c56392a38ebc5827d94fd743ead5f94db395de95
notGonim <mhmodgonim96@gmail.com>
2021-03-21 16:37:49 +0200

    ADDED the addItemToCart func in cart actions file

---
+++ client/src/redux/cart/cart-actions.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+
+export const addItemToCart = (id, quantity) => async (dispatch) => {
+    const { data } = await axios.get(`/api/product/${id}`)
+    dispatch({
+        type: 'ADD_TO_CART',
+        payload: {
+            product: data.product._id,
+            name: data.product.name,
+            price: data.product.price,
+            image: data.product.images[0].url,
+            stock: data.product.stock,
+            quantity
+        }
+    })
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+
+}
