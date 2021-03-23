15047c2cb9db6c5645390096fa5cf4f4ea5fbb9d
notGonim <mhmodgonim96@gmail.com>
2021-03-23 19:08:11 +0200

    ADDED : createProductReview func in product-control file

--- backend/controllers/product-controller.js
+++ backend/controllers/product-controller.js
@@ -90,3 +90,45 @@ export const deleteProductById = asyncError(async (req, res, next) => {
     )
 }
 )
+
+
+//Create new review   => api/review 
+export const createProductReview = asyncError(async (req, res, next) => {
+
+    const { rating, comment, productId } = req.body
+    const review = {
+        user: req.user.id,
+        name: req.user.name,
+        rating: Number(rating),
+        comment
+    }
+
+    const product = await Product.findById(productId)
+
+
+    const isReviewed = product.reviews.find(
+        r => r.user.toString() === req.user._id.toString()
+    )
+
+    if (isReviewed) {
+        product.reviews.forEach(review => {
+            if (review.user.toString() === req.user._id.toString()) {
+                review.comment = comment;
+                review.rating = rating;
+            }
+        })
+
+    } else {
+        product.reviews.push(review);
+        product.numOfReviews = product.reviews.length
+    }
+
+    product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length
+
+    await product.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true
+    })
+
+})
